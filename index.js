88aeb907336746c4489acff1b6d54cbcf450ab69
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-21 00:09:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/* 4) Define a function with one parameter, which will be a string. The function must do the following:
+a) Have a clear, descriptive name.
+b) Retrieve only the last character from strings with lengths of 3 or less.
+3 or less - subtract the last character of the string
+c) Retrieve only the first 3 characters from strings with lengths larger than 3. 
+More than 3 - subtract the first 3 characters of the string
+
+d) Use a template literal to return the phrase, "We put the '___' in '___'." Fill the first blank with the modified string, and fill the second blank
+
+
+
+function newString(string){
+  if (string > string.length || string < string.length){
+    .slice()
+  }
+}
+
+console.log(`We put the ${}' in ${}." `);//Fill the first blank with the modified string, and fill the second blank with the original string.
+
+/* Now test your function:
+e) Outside of the function, define the variable str and initialize it with a string (e.g. 'Functions rock!').
+*/
